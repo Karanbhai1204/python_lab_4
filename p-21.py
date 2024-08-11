f672f6aa8fc88e0569f613c9ebd54df82dda9c16
Karanbhai1204 <bca2022karan1674@tnraocollege.org>
2024-08-11 22:11:43 +0530

    Add files via upload

---
+++ p-21.py
@@ -0,0 +1,9 @@
+emp = {
+    "id": 1,
+    "name": "Arun Rudatala",
+    "designation": "Software Engineer",
+    "salary": 50000
+}
+del emp["designation"]
+emp["name"] = "Karan Rudatala"
+print(emp)
